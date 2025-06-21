9c390bb329e30a6bcd6e952a9e2cb914dfb8e413
carlos oliveira <carlos.sroliveira@hotmail.com>
2025-06-20 22:19:28 -0300

    Refactor useFetch hook to remove CORS proxy and improve error handling for API requests

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -4,20 +4,20 @@ import axios from 'axios';
 export const useFetch = <T = unknown>(endpoint: string): { data: T | null } => {
   const [data, setData] = useState<T | null>(null);
 
-  const CORS_PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
-  const HGBRASIL_API_BASE_URL = 'https://api.hgbrasil.com/';
+  const API_BASE_URL = 'https://api.hgbrasil.com/';
 
   useEffect(() => {
-    const fullUrl = `${CORS_PROXY_URL}${HGBRASIL_API_BASE_URL}${endpoint}`;
+    if (!API_BASE_URL) {
+      console.error(
+        'Erro: REACT_APP_API_BASE_URL não definida nas variáveis de ambiente!',
+      );
+      return;
+    }
 
-    axios
-      .get(fullUrl)
-      .then((response) => setData(response.data))
-      .catch((error) => {
-        console.error('Erro na requisição da API (via CORS proxy):', error);
-        setData(null);
-      });
-  }, [endpoint]);
+    const fullUrl = `${API_BASE_URL}${endpoint}`;
+
+    axios.get(fullUrl).then((response) => setData(response.data));
+  }, [endpoint, API_BASE_URL]);
 
   return { data };
 };
