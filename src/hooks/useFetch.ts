fae974ea2af606120af780b9c1b0e40179ed25cf
carlos oliveira <carlos.sroliveira@hotmail.com>
2025-06-20 21:43:42 -0300

    Refactor useFetch hook to utilize API_BASE_URL from environment variables and improve error handling

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,12 +1,29 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-export const useFetch = <T = unknown>(url: string): { data: T | null } => {
+export const useFetch = <T = unknown>(endpoint: string): { data: T | null } => {
   const [data, setData] = useState<T | null>(null);
 
+  const API_BASE_URL: string = process.env.REACT_APP_API_BASE_URL || '';
+
   useEffect(() => {
-    axios.get(url).then((response) => setData(response.data));
-  }, [url]);
+    if (!API_BASE_URL) {
+      console.error(
+        'Erro: REACT_APP_API_BASE_URL não definida nas variáveis de ambiente!',
+      );
+      return;
+    }
+
+    const fullUrl = `${API_BASE_URL}${endpoint}`;
+
+    axios
+      .get(fullUrl)
+      .then((response) => setData(response.data))
+      .catch((error) => {
+        console.error('Erro na requisição da API:', error);
+        setData(null);
+      });
+  }, [endpoint, API_BASE_URL]);
 
   return { data };
 };
